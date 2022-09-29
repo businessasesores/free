2256cea293e6b99d3b29e43fdcfa80266641e616
lionilustres <74267850+lionilustres@users.noreply.github.com>
2022-09-28 21:16:23 -0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -83,7 +83,7 @@ function showDomains(dominios){
 		html += "		    <p class='card-text card_text_domain'>"+ dominios[i].domain+"</p>"
 		html += "		    <h5 class='card-title card_title_domain'>"+ dominios[i].tld+"</h5>"
 		if(dominios[i].status === "AVAILABLE"){
-			html += "		    <a target='_blank' href='https://my.freenom.com/domains.php?a=availability&domain="+ dominios[i].domain +"' class='btn btn-success btn_free_domain'><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='currentColor'                   viewBox='0 0 16 16'><path d='M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z'></path></svg><span class='domain_available_text'>Registrar</span></a>"
+			html += "		    <a target='_blank' href='https://my.freenom.com/domains.php?a=availability&domain="+ dominios[i].domain +"' class='btn btn-success btn_free_domain'>                 viewBox='0 0 16 16'><path d='M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z'></path></svg><span class='domain_available_text'>Registrar</span></a>"
 		}else{
 			html += "		    <p class='not_avilable_domain'><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='currentColor' class='bi bi-x-circle' viewBox='0 0 16 16'><path d='M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z'/><path d='M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z'/></svg><span class='domain_not_available_text'>Registrado</span></p>"
 		}
